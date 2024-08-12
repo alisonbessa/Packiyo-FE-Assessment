75e0f2fbe9cdc7bf1aaef743cd8079a1f0e561ae
Alison Bessa <alisonbessa@gmail.com>
2024-08-12 11:56:26 -0300

    first commit with initial component

---
+++ app/src/components/Layout/Layout.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+import { Sidebar } from "../Sidebar";
+import { Box } from "@mui/material";
+
+export const Layout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <Box display="flex" height="100vh">
+      <Sidebar />
+      <Box component="main" flex="1" padding="20px">
+        {children}
+      </Box>
+    </Box>
+  );
+};
---
+++ app/src/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,67 @@
+import { FunctionComponent, useState } from "react";
+import {
+  Box,
+  IconButton,
+  List,
+  ListItemIcon,
+  ListItemText,
+} from "@mui/material";
+import {
+  Dashboard,
+  Inventory,
+  ListAlt,
+  KeyboardDoubleArrowLeft,
+  KeyboardDoubleArrowRight,
+} from "@mui/icons-material";
+import { useNavigate } from "@remix-run/react";
+import { SidebarContainer, SidebarItem } from "./Sidebar.styles";
+
+export const Sidebar: FunctionComponent = () => {
+  const [collapsed, setCollapsed] = useState(false);
+  const navigate = useNavigate();
+
+  const pages = [
+    { icon: <Dashboard />, label: "Summary", path: "/" },
+    { icon: <Inventory />, label: "Products", path: "/products" },
+    { icon: <ListAlt />, label: "Orders", path: "/orders" },
+  ];
+
+  const handleToggle = () => {
+    setCollapsed(!collapsed);
+  };
+
+  const handleNavigation = (path: string) => {
+    navigate(path);
+  };
+
+  return (
+    <SidebarContainer collapsed={collapsed}>
+      <Box display="flex" justifyContent="flex-end" p={1}>
+        <IconButton onClick={handleToggle}>
+          {collapsed ? (
+            <KeyboardDoubleArrowRight />
+          ) : (
+            <KeyboardDoubleArrowLeft />
+          )}
+        </IconButton>
+      </Box>
+      <List>
+        {pages.map((page) => (
+          <SidebarItem
+            key={page.label}
+            onClick={() => handleNavigation(page.path)}
+            collapsed={collapsed}
+          >
+            <ListItemIcon style={{ minWidth: 0 }}>{page.icon}</ListItemIcon>
+            {!collapsed && (
+              <ListItemText
+                style={{ margin: "0 0 0 1rem" }}
+                primary={page.label}
+              />
+            )}
+          </SidebarItem>
+        ))}
+      </List>
+    </SidebarContainer>
+  );
+};
